9d66b1d57d2f7fc1c33e299c85a2edf93ed893bf
lnmunhoz <ln.munhoz@gmail.com>
2017-11-13 23:29:16 +0700

    Add production configs for build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
+const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -11,6 +12,7 @@ module.exports = {
     rules: [{
       test: /\.(js)$/,
       use: 'babel-loader',
+      exclude: /node_modules/,
     }, {
       test: /\.(css)$/,
       use: ['style-loader', 'css-loader'],
@@ -22,3 +24,16 @@ module.exports = {
     }),
   ],
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': { 
+        NODE_ENV: 'production',
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+  );
+}
+
+module.exports = config;
